6b6e82a0de62b1476b9167aedd182ac889239c9c
Paul Sealock <psealock@gmail.com>
2018-12-04 13:24:39 +1300

    client side, variations

--- client/analytics/report/products/table-variations.js
+++ client/analytics/report/products/table-variations.js
@@ -75,15 +75,8 @@ export default class VariationsReportTable extends Component {
 		const persistedQuery = getPersistedQuery( query );
 
 		return map( data, row => {
-			const {
-				items_sold,
-				gross_revenue,
-				orders_count,
-				stock_status,
-				stock_quantity,
-				low_stock_amount,
-				product_id,
-			} = row;
+			const { items_sold, gross_revenue, orders_count, extended_info, product_id } = row;
+			const { stock_status, stock_quantity, low_stock_amount } = extended_info;
 			const name = get( row, 'extended_info.name', '' ).replace( ' - ', ' / ' );
 			const ordersLink = getNewPath( persistedQuery, 'orders', {
 				filter: 'advanced',
