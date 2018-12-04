02a7a47b6bbdaa204d3f1cc6560a35efd090e0d1
Jeff Stieler <jeff.m.stieler@gmail.com>
2018-12-03 20:01:49 -0700

    Add report stats to the `wc-api` spec.

--- client/wc-api/wc-api-spec.js
+++ client/wc-api/wc-api-spec.js
@@ -4,15 +4,20 @@
  * Internal dependencies
  */
 import orders from './orders';
+import reportStats from './reports/stats';
 
 function createWcApiSpec() {
 	return {
 		selectors: {
 			...orders.selectors,
+			...reportStats.selectors,
 		},
 		operations: {
 			read( resourceNames ) {
-				return [ ...orders.operations.read( resourceNames ) ];
+				return [
+					...orders.operations.read( resourceNames ),
+					...reportStats.operations.read( resourceNames ),
+				];
 			},
 		},
 	};
